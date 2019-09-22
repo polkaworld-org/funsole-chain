8d143dc043491773386e96749a13a4a149efd579
ennea8 <ennea8@gmail.com>
2019-09-22 10:50:41 +0800

    can store sneaker as struct

--- substratekitties/runtime/src/substratekitties.rs
+++ substratekitties/runtime/src/substratekitties.rs
@@ -1,15 +1,28 @@
 use support::{decl_storage, decl_module, StorageMap, dispatch::Result};
 use system::ensure_signed;
 
+use runtime_primitives::traits::{As, Hash};
+use parity_codec::{Encode, Decode};
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Sneaker<Hash, Balance> {
+    id: Hash,
+    price: Balance,
+}
+
+
 //pub trait Trait: system::Trait {}
 pub trait Trait: balances::Trait {}
 
 
 decl_storage! {
-    trait Store for Module<T: Trait> as KittyStorage {
+    trait Store for Module<T: Trait> as SneakerStorage {
         // Declare storage and getter functions here
         // Value: u64;
         Value: map T::AccountId => u64;
+        OwnedSneaker get(sneaker_of_owner): map T::AccountId => Sneaker<T::Hash, T::Balance>;
+
     }
 }
 
@@ -22,6 +35,18 @@ decl_module! {
 
             //<Value<T>>::put(value);
             <Value<T>>::insert(sender, value);
+
+            Ok(())
+        }
+        fn create_sneaker(origin) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            let new_sneaker = Sneaker {
+                id: <T as system::Trait>::Hashing::hash_of(&0),
+                price: <T::Balance as As<u64>>::sa(0),
+            };
+
+            <OwnedSneaker<T>>::insert(&sender, new_sneaker);
 
             Ok(())
         }
